fad1cab489b00faf69d9cb0cd531fcdcc68e7ee8
AmpF5 <lolc44t@gmail.com>
2022-12-01 20:05:52 +0100

    added user input from desktop

--- Computer_Graphics_2/MainWindow.xaml.cs
+++ Computer_Graphics_2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,17 @@ namespace Computer_Graphics_2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new();
+            openFile.Filter = "Image files| *.jpg; *.png";
+            openFile.FilterIndex = 1;
+            if (openFile.ShowDialog() == true)
+            {
+                imagePicture.Source = new BitmapImage(new Uri(openFile.FileName));
+            }
+        }
     }
+    
 }
