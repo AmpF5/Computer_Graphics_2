97bfe9d9ff6851879bdd44fc6b97ec40422d6655
AmpF5 <lolc44t@gmail.com>
2022-12-05 13:21:23 +0100

    added smoothing_filter

---
+++ Computer_Graphics_2/SmoothingFilter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+using Color = System.Drawing.Color;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+
+namespace Computer_Graphics_2
+{
+    internal class SmoothingFilter
+    {
+        private static Bitmap _bitmap;
+
+        public SmoothingFilter(BitmapImage bitmapImage)
+        {
+            _bitmap = BitmapImage2Bitmap(bitmapImage);
+        }
+
+        public ImageSource Test()
+        {
+            int w = _bitmap.Width;
+            int h = _bitmap.Height;
+            BitmapData image_data = _bitmap.LockBits(
+                new Rectangle(0, 0, w, h),
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            int bytes = image_data.Stride * image_data.Height;
+            byte[] buffer = new byte[bytes];
+            byte[] result = new byte[bytes];
+            Marshal.Copy(image_data.Scan0, buffer, 0, bytes);
+            _bitmap.UnlockBits(image_data);
+            for (int i = 2; i < w - 2; i++)
+            {
+                for (int j = 2; j < h - 2; j++)
+                {
+                    int p = i * 3 + j * image_data.Stride;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        List<int> vals = new List<int>();
+                        for (int xkernel = -2; xkernel < 3; xkernel++)
+                        {
+                            for (int ykernel = -2; ykernel < 3; ykernel++)
+                            {
+                                int kernel_p = k + p + xkernel * 3 + ykernel * image_data.Stride;
+                                vals.Add(buffer[kernel_p]);
+                            }
+                        }
+                        result[p + k] = (byte)(vals.Sum() / vals.Count);
+                    }
+                }
+            }
+            Bitmap res_img = new Bitmap(w, h);
+            BitmapData res_data = res_img.LockBits(
+                new Rectangle(0, 0, w, h),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format24bppRgb);
+            Marshal.Copy(result, 0, res_data.Scan0, bytes);
+            res_img.UnlockBits(res_data);
+
+            return BitmapToImageSource(res_img);
+            
+        }
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+        private static Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                Bitmap bitmap = new(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+    }
+}
